7fb675df22a76a244f29ffe9fec9df05c5701e1b
Jenyay <jenyay.ilin@gmail.com>
2016-03-20 00:07:15 +0300

    TexEquation. MimeTex replaced to KaTeX

--- plugins/texequation/texequation/tokentex.py
+++ plugins/texequation/texequation/tokentex.py
@@ -1,34 +1,60 @@
 # -*- coding: UTF-8 -*-
 
+from abc import ABCMeta, abstractmethod
 import os
 import os.path
+import shutil
 
+from outwiker.core.system import getOS
 from outwiker.libs.pyparsing import QuotedString
-from texrender import getTexRender
 from outwiker.pages.wiki.thumbnails import Thumbnails
 
 
 class TexFactory (object):
     @staticmethod
-    def make (parser):
-        return TexToken(parser).getToken()
+    def makeInlineTexToken (parser):
+        return InlineTexToken(parser).getToken()
 
 
-class TexToken (object):
-    """
-    Класс токена для разбора формул
-    """
-    texStart = "{$"
-    texEnd = "$}"
+    @staticmethod
+    def makeBigTexToken (parser):
+        return BigTexToken(parser).getToken()
+
+
+class BaseTexToken (object):
+    __metaclass__ = ABCMeta
 
     def __init__ (self, parser):
         self.parser = parser
+        self._headers = [u'<link rel="stylesheet" href="__attach/__thumb/katex/katex.min.css">',
+                         u'<script src="__attach/__thumb/katex/katex.min.js"></script>']
+        self._divIndex = 0
+        self._divTemplate = u'''<span class="{classname}" id="{idname}-{index}"></span>
+<script>
+var element = document.getElementById("{idname}-{index}");
+katex.render("{code}", element, {{ displayMode: {displayMode}, throwOnError: false }});
+</script>'''
 
 
     def getToken (self):
-        return QuotedString (TexToken.texStart,
-                             endQuoteChar = TexToken.texEnd,
-                             multiline = True).setParseAction(self.makeTexEquation)("tex")
+        return QuotedString (self.texStart,
+                             endQuoteChar = self.texEnd,
+                             multiline = True).setParseAction(self.makeTexEquation)("inlinetex")
+
+
+    @abstractmethod
+    def _getDisplayParam (self):
+        pass
+
+
+    @abstractmethod
+    def _getClassName (self):
+        pass
+
+
+    @abstractmethod
+    def _getIdName (self):
+        pass
 
 
     def makeTexEquation (self, s, l, t):
@@ -39,18 +65,78 @@ class TexToken (object):
         thumb = Thumbnails(self.parser.page)
 
         try:
-            path = thumb.getThumbPath (True)
+            thumb_path = thumb.getThumbPath (True)
         except IOError:
             return u"<b>{}</b>".format(_(u"Can't create thumbnails directory"))
 
-        tex = getTexRender (path)
+        if self._headers[0] not in self.parser.head:
+            map (lambda head: self.parser.appendToHead (head), self._headers)
+            katex_path = os.path.join (thumb_path, u'katex')
 
-        try:
-            image_fname = tex.makeImage (eqn)
-        except IOError:
-            return u"<b>{}</b>".format(_(u"Can't create image file"))
+            if os.path.exists (katex_path):
+                try:
+                    shutil.rmtree (katex_path)
+                except shutil.Error:
+                    return u"<b>{}</b>".format(_(u"Can't remove KaTeX library"))
+            try:
+                shutil.copytree (self._getKaTeXPath(), katex_path)
+            except shutil.Error:
+                return u"<b>{}</b>".format(_(u"Can't copy KaTeX library"))
 
-        image_path = os.path.join (Thumbnails.getRelativeThumbDir(), image_fname)
-        result = u'<img src="{image}"/>'.format (image=image_path.replace ("\\", "/"))
+        eqn = eqn.replace ('\\', '\\\\')
+        eqn = eqn.replace ('"', '\\"')
+        # eqn = eqn.encode('ascii', 'xmlcharrefreplace')
 
+        result = self._divTemplate.format (index = self._divIndex,
+                                           code = eqn,
+                                           displayMode = self._getDisplayParam(),
+                                           classname = self._getClassName(),
+                                           idname = self._getIdName())
+        self._divIndex += 1
         return result
+
+
+    def _getKaTeXPath (self):
+        """
+        Get path to KaTeX library
+        """
+        katexpath = unicode (os.path.join (os.path.dirname (__file__), "tools", "katex"), getOS().filesEncoding)
+        return katexpath
+
+
+class InlineTexToken (BaseTexToken):
+    """
+    Класс токена для разбора формул
+    """
+    texStart = "{$"
+    texEnd = "$}"
+
+    def _getDisplayParam (self):
+        return u'false'
+
+
+    def _getClassName (self):
+        return u'tex-inline'
+
+
+    def _getIdName (self):
+        return u'tex-inline'
+
+
+class BigTexToken (BaseTexToken):
+    """
+    Класс токена для разбора формул
+    """
+    texStart = "{$$"
+    texEnd = "$$}"
+
+    def _getDisplayParam (self):
+        return u'true'
+
+
+    def _getClassName (self):
+        return u'tex-big'
+
+
+    def _getIdName (self):
+        return u'tex-big'
